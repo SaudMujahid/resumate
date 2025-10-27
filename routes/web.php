731bd310aaf1433ef25600771fd9b9318b68cf95
Saud Mujahid <saudbinmujahid@gmail.com>
2025-10-28 00:35:59 +0600

    added features link

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/templates', function () {
     return view('test');
 });
 
+Route::get('/features', function () {
+    return view('features');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
