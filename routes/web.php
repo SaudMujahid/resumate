05eb6dfde8c98588e821dbf24d7fa3dd248900d2
Saud Mujahid <saudbinmujahid@gmail.com>
2025-11-13 22:11:49 +0600

    new routes for resumebuilder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ResumeBuilderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -26,6 +27,18 @@ Route::get('/features', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('ResumeBuilder', ResumeBuilderController::class);
+
+// New routes for resume processing
+Route::post('/generate-resume', [ResumeBuilderController::class, 'generate'])->name('resume.generate');
+
+Route::get('/resume/modern', [ResumeBuilderController::class, 'showModern'])->name('resume.modern');
+Route::get('/resume/chronological', [ResumeBuilderController::class, 'showChronological'])->name('resume.chronological');
+Route::get('/resume/minimal', [ResumeBuilderController::class, 'showMinimal'])->name('resume.minimal');
+
+Route::post('/resume/update', [ResumeBuilderController::class, 'update'])->name('resume.update');
+Route::post('/resume/download-pdf', [ResumeBuilderController::class, 'downloadPDF'])->name('resume.download');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
